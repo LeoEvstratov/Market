1f840c14c5ec106ce09bf54a4d4809dd398cf2fb
Lev Evstratov <evstratovlg@gmail.com>
2020-10-27 23:19:36 +0300

    some ideas to develop project

--- Market/src/main/java/com/evstratov/market/MarketApplication.java
+++ Market/src/main/java/com/evstratov/market/MarketApplication.java
@@ -7,8 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MarketApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MarketApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MarketApplication.class, args);
+    }
+//    some ideas to improve this project
+//todo manager profile (all orders with user addresses and phones and status change selector)
+//todo	user profile (my orders)
+//todo	search with multiple parameters
+// todo   address (kladr??) and phone validation in order
+//    todo mass import/export products from to excel/txt/etc...
+//    todo order statistics
 }
